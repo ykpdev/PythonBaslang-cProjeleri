e748482f820b48e647218da2e24da2fac53b06bc
Yakup Kurt <81492142+ykpdev@users.noreply.github.com>
2023-08-06 20:38:38 +0300

    Add files via upload

--- SayıArtırmaAzaltma.py
+++ SayıArtırmaAzaltma.py
@@ -1,6 +1,23 @@
 """Kullanıcı tarafınfan + girildiği zaman sayı arttırma - girildiği zaman sayı azaltma işlemi yapılmaktadır
 Bunun için kullanıcıdan sürekli olarak + ve - basması beklenilmektedir 
 Başka bir karaktere basıldığında program çıkış yapacaktır.
-
-
 """
+# while döngüsü kullanmamız gerekir sonzuz bir döngü oluşturmak için
+"""Bir sayı tanımlanması yapılır kullanıcıdan bu sayıyı arttırma ve azaltma işlemi yapılması istenmektedir
+farklı bir karektere basılırsa çıkış işlemi yapılacaktır
+"""
+
+sayı=0
+
+
+
+while True:
+    durum=input()   
+    if(durum=="+"):
+        sayı=sayı+1;
+    elif(durum=="-"):
+        sayı=sayı-1;
+    else:
+        quit()
+
+    print(sayı)
